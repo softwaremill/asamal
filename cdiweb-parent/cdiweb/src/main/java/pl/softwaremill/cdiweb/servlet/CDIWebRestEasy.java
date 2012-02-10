c403790a8e472c26528b17f42ac012f186aaf60b
szimano <szimano@szimano.org>
2012-02-10 11:18:21 +0200

    POST using JAX-RS (missing files)

---
+++ cdiweb-parent/cdiweb/src/main/java/pl/softwaremill/cdiweb/servlet/CDIWebRestEasy.java
@@ -0,0 +1,11 @@
+package pl.softwaremill.cdiweb.servlet;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * User: szimano
+ */
+@ApplicationPath("post")
+public class CDIWebRestEasy extends Application {
+}
