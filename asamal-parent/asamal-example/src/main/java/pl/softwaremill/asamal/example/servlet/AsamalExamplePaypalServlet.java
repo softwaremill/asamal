9fcc967f35c7bed7a3e004296b3e9c192f115aa4
szimano <szimano@szimano.org>
2012-04-11 09:00:30 +0200

    - logs for paypal

--- asamal-parent/asamal-example/src/main/java/pl/softwaremill/asamal/example/servlet/AsamalExamplePaypalServlet.java
+++ asamal-parent/asamal-example/src/main/java/pl/softwaremill/asamal/example/servlet/AsamalExamplePaypalServlet.java
@@ -1,5 +1,7 @@
 package pl.softwaremill.asamal.example.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.softwaremill.asamal.example.logic.conf.ConfigurationBean;
 import pl.softwaremill.asamal.example.model.conf.Conf;
 import pl.softwaremill.asamal.example.servlet.paypal.PaypalErrorProcessor;
@@ -12,13 +14,19 @@ import pl.softwaremill.common.util.dependency.D;
 
 import javax.inject.Inject;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Servlet to handle paypal notifications
  */
 @WebServlet(urlPatterns = "/paypal")
 public class AsamalExamplePaypalServlet extends IPNServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(AsamalExamplePaypalServlet.class);
 
     @Inject
     private ConfigurationBean configurationBean;
@@ -40,8 +48,20 @@ public class AsamalExamplePaypalServlet extends IPNServlet {
             @Override
             protected <T extends PayPalProcessor> T createNewInstance(Class<T> processorClass) throws
                     IllegalAccessException, InstantiationException {
-                return D.inject(processorClass);
+                try {
+                    return D.inject(processorClass);
+                } catch (RuntimeException e) {
+                    // otherwise just try the "normal" way, if the bean cannot be injected
+                    return super.createNewInstance(processorClass);
+                }
             }
         };
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("New PayPal POST");
+        super.doPost(request, response);
+        log.info("Finished PayPal POST");
     }
 }
